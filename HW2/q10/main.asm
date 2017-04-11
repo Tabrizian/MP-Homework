83f08685f4183a48ef9c9b7feb34c87e276e1c1b
Iman Tabrizian <Tabrizian@outlook.com>
2017-04-11 23:20:01 +0430

    Adds some number

--- HW2/q10/main.asm
+++ HW2/q10/main.asm
@@ -10,53 +10,36 @@
 ; DEFINITIONS
 ;====================================================================
 .def no_of_bytes=R16
-.def start_index=R17
-.def end_index=R18
 .def current_max=R19
 .def current_number=R20
 .def current_index=R21
 .def current_max_index=R22
 .def temp=R23
+.def count=R24
+.def temp_2=R25
 
 ;====================================================================
 ; VARIABLES
 ;====================================================================
 
+.cseg
+ARRAY:
+.db 0x01, 0x02, 0x03, 0x04, 0x05, 0x06
 ;====================================================================
 ; RESET and INTERRUPT VECTORS
 ;====================================================================
 
       ; Reset Vector
-      ldi zh, 0x01
-      ldi xh, 0x01
-      ldi xl, 0x00
-      ldi R25, 0x00
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
-      inc R25
-      inc xl
-      st X, R25
+      ldi zh, high(ARRAY)
+      ldi zl, low(ARRAY)
       rjmp  Start
 
 ;====================================================================
 ; CODE SEGMENT
 ;====================================================================
 Max:
-      mov current_index, start_index
+      mov current_index, zl
+      ldi temp, 0x00
 MaxLoop:
       mov zl, current_index
       ld current_number, Z
@@ -66,25 +49,48 @@ MaxLoop:
       mov current_max_index, current_index
 skip:
       inc current_index
-      cp current_index, end_index
+      inc temp
+      cp temp, no_of_bytes
       brlo MaxLoop
 ret
 
 Start:
-      ldi no_of_bytes, 0x64 ; No of bytes to sort
-      ldi start_index, 0x00 ; Start Index
-      ldi end_index, 0x09 ; End Index
+      ldi no_of_bytes, 0x06 ; No of bytes to sort
+      ldi count, 0
+      mov temp, no_of_bytes
+
+
+      ldi zh, 0x00
+      ldi zl, low(ARRAY)
+      ldi xh, 0x01
+      ldi xl, low(ARRAY)
+Begin:
+      lpm temp_2, z
+      st x, temp_2
+      dec temp
+      inc zl
+      inc xl
+      cpi temp, 0
+      brne Begin
+
+
+      ldi zh, 0x01
+      ldi zl, low(ARRAY)
+      ldi xh, 0x01
 Sort:
       ldi current_max, 0x00 ; Assumed Max index
 
+      movw x, z
       rcall max
-      mov zl, start_index
+      movw z, x
       ld temp, z
-      st Z, current_max
-      mov zl, current_max_index
-      st Z, temp
-      inc start_index
-      cp start_index, end_index
+      st z, current_max
+
+      mov xl, current_max_index
+      st x, temp
+      inc zl
+      inc count
+      cp count, no_of_bytes
       brlo Sort
 
       ; Write your code here
